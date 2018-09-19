d128f5a4e265a3aa9c28930afa96befb2e2ae0dc
Sergio Souza Novak <serginhosnovak@hotmail.com>
2018-09-19 11:15:34 -0300

    adicionado número formatado

--- NumeroTelefone.ps1
+++ NumeroTelefone.ps1
@@ -1,16 +1,12 @@
-
-
 # Regex para Números de Telefones no Brasil 
-
 while($true){
 
 $telefone = Read-Host 'Entre com o telefone'
 $pattern1 = '^\d{11}$'
 $pattern2 = '^\([\d]{2}\)\d{9}$'
+$pattern3 = '^\([\d]{2}\)\d{5}-\d{4}$'
 
- 
- if($telefone -match $pattern1 -or $telefone -match $pattern2-or $telefone -match $pattern3){
+ if($telefone -match $pattern1 -or $telefone -match $pattern2 -or $telefone -match $pattern3){
     Write-Host "string aceita"
  }
 }
-
